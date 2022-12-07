3d73fd62f81747e68a26af46dad9c59855d1fa40
matejpolj <42142798+matejpolj@users.noreply.github.com>
2022-12-07 18:23:02 +0100

    day 6 finished

--- day6.py
+++ day6.py
@@ -1,7 +1,6 @@
 
 def shiftBuff(L, inp):
     I = []
-    print(L, len(L), inp)
     for i in range(1, len(L)):
         I.append(L[i])
     I.append(inp)
@@ -11,30 +10,29 @@ def shiftBuff(L, inp):
 def checkComp(L, inp):
     for i in range(1, len(L)):
         if (L[i] == inp):
-            print(len(L)-1-i)
-            return (len(L)-i)
+            return 1
+    return 0
+
+
+def checkInBuf(L):
+    for i in range(0, len(L)):
+        for j in range(i+1, len(L)):
+            if (L[i] == L[j]):
+                return 1
     return 0
 
 
 with open('input06.txt') as f:
     lines = f.readlines()
     lin = lines[0]
-    buff = ['', '', '',lin[0]]
+    buff = [lin[0], lin[1], lin[2], lin[3], lin[4], lin[5], lin[6], lin[7], lin[8], lin[9], lin[10], lin[11], lin[12], lin[13]]
     tmp = []
     ind = 0
-    i = 1
-    while (i < len(lin)):
-        print(buff, lin[i], '\n', i)
-        ints = checkComp(buff, lin[i])
-        print(ints)
-        if ((ints == 0) and (i > 3)):
+    for i in range(14, len(lin)):
+        if ((checkInBuf(buff) == 0)):
            ind = i
            break
         else:
             tmp = shiftBuff(buff, lin[i]) 
             buff = tmp
-            if(i > 0):
-                i += ints - 1
-        i += 1
-        print(i, 'a')
     print(ind)

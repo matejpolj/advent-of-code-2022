06878976e3e2d7b92779b687ae22bc297556812f
matejpolj <42142798+matejpolj@users.noreply.github.com>
2022-12-08 15:44:51 +0100

    Update day8.py

--- day8.py
+++ day8.py
@@ -1,5 +1,7 @@
 
 def isVisible(L, ind, jnd):
+    if ((ind == 0) or (jnd == 0) or (ind == (len(L)-1)) or (jnd == (len(L[0])-1))):
+        return 1
     get_height = int(L[ind][jnd])
     num = 0
     for i in range(0, len(L)-1):
@@ -7,12 +9,28 @@ def isVisible(L, ind, jnd):
            num += 1
         if (i == ind):
             if (num == (ind-1)):
-                return 1 
+                return 1
+            num = 0 
+    if (num == (len(L)-ind)):
+        return 1
+    num = 0
     for j in range(0, len(L[0])-1):
         if (int(L[ind][j]) < get_height):
-            return 1
+            num += 1
+        if (j == jnd):
+            if (num == (jnd-1)) :
+                return 1
+            num = 0
+    if (num == (len(L[0])-jnd)):
+        return 1
     return 0
 
 
 with open('input08.txt') as f:
     lines = f.readlines()
+    sum = 0
+    for i in range(0, len(lines)):
+        for j in range(0, len(lines[0])-1):
+            print(isVisible(lines, i, j), i, j)
+            sum  += isVisible(lines, i, j)
+    print(sum)

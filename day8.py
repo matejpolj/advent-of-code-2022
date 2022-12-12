6b136c6a5b429bb60f6e6ccd5e246f120ed7b1c3
matejpolj <42142798+matejpolj@users.noreply.github.com>
2022-12-12 13:19:42 +0100

    part one day 8 finished

--- day8.py
+++ day8.py
@@ -1,36 +1,52 @@
 
-def isVisible(L, ind, jnd):
-    if ((ind == 0) or (jnd == 0) or (ind == (len(L)-1)) or (jnd == (len(L[0])-1))):
+def isVisibleTop(L, ind, jnd):
+    if (ind == 0):
         return 1
     get_height = int(L[ind][jnd])
-    num = 0
-    for i in range(0, len(L)-1):
-        if (int(L[i][jnd]) < get_height):
-           num += 1
-        if (i == ind):
-            if (num == (ind-1)):
-                return 1
-            num = 0 
-    if (num == (len(L)-ind)):
-        return 1
-    num = 0
-    for j in range(0, len(L[0])-1):
-        if (int(L[ind][j]) < get_height):
-            num += 1
-        if (j == jnd):
-            if (num == (jnd-1)) :
-                return 1
-            num = 0
-    if (num == (len(L[0])-jnd)):
-        return 1
-    return 0
+    for i in range(0, ind):
+        if (int(L[i][jnd]) >= get_height):
+            return 0
+    print("vt", ind, jnd)
+    return 1
 
+
+def isVisibleBottom(L, ind, jnd):
+    if (ind == (len(L)-1)):
+        return 1
+    get_height = int(L[ind][jnd])
+    for i in range(ind+1, len(L)):
+        if (int(L[i][jnd]) >= get_height):
+            return 0
+    print("vb", ind, jnd)
+    return 1
+
+
+def isVisibleLeft(L, ind, jnd):
+    if (jnd == 0):
+        return 1
+    get_height = int(L[ind][jnd])
+    for j in range(0, jnd):
+        if (int(L[ind][j]) >= get_height):
+            return 0
+    print("vl", ind, jnd)
+    return 1
+
+
+def isVisibleRight(L, ind, jnd):
+    if (jnd == (len(L[0])-2)):
+        return 1
+    get_height = int(L[ind][jnd])
+    for j in range(jnd+1, len(L[0])-1):
+        if (int(L[ind][j]) >= get_height):
+            return 0
+    print("vr", ind, jnd)
+    return 1
 
 with open('input08.txt') as f:
     lines = f.readlines()
     sum = 0
     for i in range(0, len(lines)):
         for j in range(0, len(lines[0])-1):
-            print(isVisible(lines, i, j), i, j)
-            sum  += isVisible(lines, i, j)
+            if (isVisibleTop(lines, i, j) or isVisibleBottom(lines, i, j) or isVisibleLeft(lines, i, j) or isVisibleRight(lines, i, j)):
+                sum  += 1
     print(sum)

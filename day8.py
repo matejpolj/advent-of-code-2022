2f48d09d50ab9f36766ad66e78cde329ebade469
matejpolj <42142798+matejpolj@users.noreply.github.com>
2022-12-12 14:18:34 +0100

    Update day8.py

--- day8.py
+++ day8.py
@@ -6,7 +6,6 @@ def isVisibleTop(L, ind, jnd):
     for i in range(0, ind):
         if (int(L[i][jnd]) >= get_height):
             return 0
-    print("vt", ind, jnd)
     return 1
 
 
@@ -17,7 +16,6 @@ def isVisibleBottom(L, ind, jnd):
     for i in range(ind+1, len(L)):
         if (int(L[i][jnd]) >= get_height):
             return 0
-    print("vb", ind, jnd)
     return 1
 
 
@@ -28,7 +26,6 @@ def isVisibleLeft(L, ind, jnd):
     for j in range(0, jnd):
         if (int(L[ind][j]) >= get_height):
             return 0
-    print("vl", ind, jnd)
     return 1
 
 
@@ -39,8 +36,19 @@ def isVisibleRight(L, ind, jnd):
     for j in range(jnd+1, len(L[0])-1):
         if (int(L[ind][j]) >= get_height):
             return 0
-    print("vr", ind, jnd)
     return 1
+
+
+def viewingTop(L, ind, jnd):
+    if (jnd == (len(L[0])-2)):
+        return 0
+    sum = 0
+    get_height = int(L[ind][jnd])
+    for j in range(jnd+1, len(L[0])-1):
+        if (int(L[ind][j]) >= get_height):
+            return 0
+    return 1
+
 
 with open('input08.txt') as f:
     lines = f.readlines()
@@ -49,4 +57,4 @@ with open('input08.txt') as f:
         for j in range(0, len(lines[0])-1):
             if (isVisibleTop(lines, i, j) or isVisibleBottom(lines, i, j) or isVisibleLeft(lines, i, j) or isVisibleRight(lines, i, j)):
                 sum  += 1
-    print(sum)
+    print("Sum is:", sum)

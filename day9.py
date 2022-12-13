372d760ab8070eddf774852f866bdc1f1f7ff160
matejpolj <42142798+matejpolj@users.noreply.github.com>
2022-12-13 22:07:29 +0100

    done part 1 of day 9

--- day9.py
+++ day9.py
@@ -3,16 +3,17 @@ def decodeInp(L):
     x = 0
     y = 0
     if (L[0] == 'U'):
-        y = int(L[2])
+        y = int(L[2:len(L)])
     elif (L[0] == 'D'):
-        y = -int(L[2])
+        y = -int(L[2:len(L)])
     elif (L[0] == 'L'):
-        x = -int(L[2])
+        x = -int(L[2:len(L)])
     elif (L[0] == 'R'):
-        x = int(L[2])
+        x = int(L[2:len(L)])
     else:
         x = 0
         y = 0
+    print(L, x, y)
     return x, y
 
 
@@ -28,7 +29,6 @@ class Head:
         i, j = decodeInp(L)
         self.x = self.x + i
         self.y = self.y + j
-        print(self)
         return i, j
 
 
@@ -46,22 +46,10 @@ class Tail:
             if ((self.x == self.pos[i][0]) and (self.y == self.pos[i][1])):
                 return
         self.pos.append([self.x, self.y])
-        print(H)
         return
 
     def followHead(self, H):
         if ((abs(H.x - self.x) > 1) or (abs(H.y - self.y) > 1)):
-            #if (abs(H.x - self.x) == 0):
-            #    if (H.y > self.y):
-            #        self.y += 1
-            #    else:
-            #        self.y -= 1
-            #elif (abs(H.y - self.y) == 0):
-            #    if (H.x > self.x):
-            #        self.x += 1
-            #    else:
-            #        self.x -= 1
-            #else:
             if (H.x > self.x):
                 self.x += 1
             elif (H.x < self.x):
@@ -71,16 +59,14 @@ class Tail:
             elif (H.y < self.y):
                 self.y -= 1
         self.storePosition(H)
-        print(self)
             
 
 with open('input09.txt') as f:
     lines = f.readlines()
-    print(lines)
     head = Head()
     tail = Tail()
     for i in range(0, len(lines)):
         x, y = head.move(lines[i])
-        for i in range(0, (x + y)):
+        for i in range(0, abs(x + y)):
             tail.followHead(head)
-    print(len(tail.pos), tail.pos)
+    print(len(tail.pos))

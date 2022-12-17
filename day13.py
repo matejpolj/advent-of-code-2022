59c05c4758f00e02410ef9f1309ae1d6280b5e92
matejpolj <42142798+matejpolj@users.noreply.github.com>
2022-12-17 19:47:39 +0100

    Update day13.py

--- day13.py
+++ day13.py
@@ -5,7 +5,8 @@ def main():
     input_text = [list(r) for r in open("input12.txt", 'r').read().splitlines()]
     sum = 0
     for i in (len(input_text)//3):
-        
+        for j in range(max(len(input_text), len(input_text[i+1]))):
+            if ((int(input_text[i][j])) and (input_text))
 
 if __name__ == '__main__':
     sys.exit(main())

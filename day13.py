acf4ddfb2904df2fe492db0f0c3b58d582140f5c
matejpolj <42142798+matejpolj@users.noreply.github.com>
2022-12-17 20:25:32 +0100

    Update day13.py

--- day13.py
+++ day13.py
@@ -2,11 +2,21 @@ import sys
 
 
 def main():
-    input_text = [list(r) for r in open("input12.txt", 'r').read().splitlines()]
+    input_text = [list(r) for r in open("input13.txt", 'r').read().splitlines()]
     sum = 0
-    for i in (len(input_text)//3):
-        for j in range(max(len(input_text), len(input_text[i+1]))):
-            if ((int(input_text[i][j])) and (input_text))
+    def convertToList(inp):
+        L = []
+        for i in range(len(inp)):
+            L.append(list(inp[i]))
+        return L
+    orig_list = convertToList(input_text)
+    def compareElement(a, b):
+        if (a.isnumeric() and b.isnumeric()):
+            return 1
+        elif (a == '[' and b == '['):
+            return 2
+        el
+    print(orig_list)
 
 if __name__ == '__main__':
     sys.exit(main())

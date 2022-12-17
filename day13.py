b9e0e331f1d40a52788d8fba3d67cb76af076a5a
matejpolj <42142798+matejpolj@users.noreply.github.com>
2022-12-18 00:11:45 +0100

    day 13 finished

--- day13.py
+++ day13.py
@@ -64,7 +64,6 @@ def main():
         return 4
     def checkCorrect(inp):
         n = 0
-        print( '\n')
         for i in range(0, len(inp), 2):
             up = 0
             down = 0
@@ -90,8 +89,42 @@ def main():
                 else:
                     continue
         return n
+    def returnCorrect(inp):
+        for i in range(len(inp)):
+            if (inp[i] == '[' and inp[i+1] == ']'):
+                return 0
+            elif (type(inp[i]) == int):
+                return inp[i]
+    def organizePackets(inp):
+        L = []
+        num = returnCorrect(inp[0])
+        L.append(num)
+        for i in range(1, len(inp)):
+            t = 0
+            num = returnCorrect(inp[i])
+            for j in range(len(L)):
+                if ((L[j] > num)):
+                    L.insert(j, num)
+                    t = 1
+                    break
+            if (t == 0):
+                L.append(num)
+        return L
     sum = checkCorrect(fitted_list)
-    print(sum)
+    fitted_list.append(['[', 2, ']'])
+    fitted_list.append(['[', 6, ']'])
+    def findMultipliers(inp):
+        two, six = 0, 0
+        for i in range(len(inp)):
+            if (inp[i] == 2 and two == 0):
+                two = i + 1
+            if (inp[i] == 6 and six == 0):
+                six = i + 1
+        print(two, six)
+        return two*six
+    pack = organizePackets(fitted_list)
+    final = findMultipliers(pack)
+    print(sum, pack, final)
 
 if __name__ == '__main__':
     sys.exit(main())

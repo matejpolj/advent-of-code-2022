8ef7b31b6f56ec774f7373743cefa4a4544c5959
matejpolj <42142798+matejpolj@users.noreply.github.com>
2022-12-17 18:57:43 +0100

    Update day13.py

--- day13.py
+++ day13.py
@@ -2,8 +2,10 @@ import sys
 
 
 def main():
-    
-
+    input_text = [list(r) for r in open("input12.txt", 'r').read().splitlines()]
+    sum = 0
+    for i in (len(input_text)//3):
+        
 
 if __name__ == '__main__':
     sys.exit(main())
